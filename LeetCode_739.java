61a68f3e1b23f7505cf7f1c6c0e8022d7217321f
Nomadic4u <2910887794@qq.com>
2024-06-03 10:33:35 +0800

    2024.6.1

--- LeetCode_739.java
+++ LeetCode_739.java
@@ -1,22 +1,20 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Stack;
+import java.util.LinkedList;
 
 public class LeetCode_739 {
     class Solution {
         public int[] dailyTemperatures(int[] temperatures) {
             int[] res = new int[temperatures.length];
-            Deque<Integer> stack = new ArrayDeque<>();
+            Deque<Integer> stack = new LinkedList<>();
             for (int i = 0; i < temperatures.length; i++) {
-                if (stack.isEmpty())
-                    stack.push(i);
-                else {
-                    while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                if (!stack.isEmpty()) {
+                    while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
                         res[stack.peek()] = i - stack.peek();
                         stack.pop();
                     }
-                    stack.push(i);
                 }
+                stack.push(i);
             }
             return res;
         }
