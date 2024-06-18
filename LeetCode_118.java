5b602970710f7643efe59162860b54b5055e043e
Nomadic4u <2910887794@qq.com>
2024-06-18 10:39:33 +0800

    2024.6.18

--- LeetCode_118.java
+++ LeetCode_118.java
@@ -4,30 +4,23 @@ import java.util.List;
 public class LeetCode_118 {
     class Solution {
         public List<List<Integer>> generate(int numRows) {
-            List<Integer> list1 = new ArrayList<>();
-            list1.add(0);
-            list1.add(1);
-            list1.add(0);
-            List<List<Integer>> res = new ArrayList<>();
-            res.add(list1);
-            List<Integer> last = new ArrayList<>();
-            last = list1;
-            for(int i = 2; i <= numRows; i ++) {
-                List<Integer> list2 = new ArrayList<>();
-                list2.add(0);
-                int len = last.size();
-                for(int j = 0; j < len - 1; j ++) {
-                    list2.add(last.get(j) + last.get(j + 1));
+            List<List<Integer>> list = new ArrayList<>();
+            int[][] str = new int[numRows + 1][numRows + 1];
+            for(int i = 1; i <= numRows; i ++) {
+                str[i][1] = 1;
+                str[i][i] = 1;
+            }
+
+            for(int i = 1; i <= numRows; i ++) {
+                List<Integer> temp = new ArrayList<>();
+                temp.add(str[i][1]);
+                for(int j = 2; j <= i; j ++) {
+                    str[i][j] = str[i - 1][j - 1] + str[i - 1][j];
+                    temp.add(str[i][j]);
                 }
-                list2.add(0);
-                res.add(list2);
-                last = list2;
+                list.add(temp);
             }
-            for(int i =0; i < res.size(); i ++) {
-                res.get(i).remove(res.get(i).size() - 1);
-                res.get(i).remove(0);
-            }
-            return res;
+            return list;
         }
     }
 }
--- LeetCode_394.java
+++ LeetCode_394.java
@@ -1,17 +1,14 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Stack;
 
 public class LeetCode_394 {
     class Solution {
         public String decodeString(String s) {
-            Deque<Integer> nums = new ArrayDeque<>();
-            Deque<String> str = new ArrayDeque<>();
+            Stack<Integer> stack = new Stack<>();
+            StringBuilder sb = new StringBuilder();
             for (int i = 0; i < s.length(); i++) {
-                if(s.charAt(i) >= '0' && s.charAt(i) <= '9')
-                    nums.addLast(i - '0');
-                else {
-                    if()
-                }
+
             }
         }
     }
