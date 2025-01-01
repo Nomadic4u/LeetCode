872e2b95ceb70c135c7c2a9768e31683c35aa15d
Nomadic4u <2910887794@qq.com>
2025-01-01 15:47:05 +0800

    2025.1.1

--- Main.java
+++ Main.java
@@ -1,15 +1,25 @@
-import java.io.IOException;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-
+    public static List<Integer> solution(List<Integer> a, List<Integer> b) {
+        // PLEASE DO NOT MODIFY THE FUNCTION SIGNATURE
+        // write code here
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Integer count : a) {
+            if(b.contains(count))
+                list.add(count);
+        }
+        Collections.sort(list, (x, y) -> y - x);
+        return list;
     }
 
-    public int search(int[] nums, int num) {
-        int left = 0, right = nums.length - 1;
-        while ()
+    public static void main(String[] args) {
+        System.out.println(solution(Arrays.asList(1, 2, 3, 7), Arrays.asList(2, 5, 7)).equals(Arrays.asList(7, 2)));
+        System.out.println(solution(Arrays.asList(1, 4, 8, 10), Arrays.asList(2, 4, 8, 10)).equals(Arrays.asList(10, 8, 4)));
+        System.out.println(solution(Arrays.asList(3, 5, 9), Arrays.asList(1, 4, 6)).equals(Collections.emptyList()));
+        System.out.println(solution(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3)).equals(Arrays.asList(3, 2, 1)));
     }
-
 }
